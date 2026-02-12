3a286b46dc967bfd0f2310be8c21632c83d618e7
Tim Terrance <timothy.terrance@sdgku.edu>
2026-02-11 19:34:12 -0600

    Initial Commit

---
+++ LemonRestaurant-ch7/AboutView.swift
@@ -0,0 +1,21 @@
+//
+//  AboutView.swift
+//  LemonRestaurant-ch7
+//
+//  Created by Timothy Terrance on 2/9/26.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text("About Little Lemon")
+            .font(.largeTitle)
+        .foregroundColor(.green)
+        
+    }
+}
+
+#Preview {
+    AboutView()
+}
